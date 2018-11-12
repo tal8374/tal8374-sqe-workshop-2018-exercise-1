4a35faa4bdb3a276186893d5245b0ccd50d8c57f
tal8374 <talivanov@gmail.com>
2018-11-12 17:11:02 +0200

    Handling with variable variable complex declaration

---
+++ src/js/value-expression-handler.js
@@ -0,0 +1,22 @@
+function ValueExpression(valueExpression) {
+    this.valueExpression = valueExpression;
+}
+
+ValueExpression.prototype.getValue = function () {
+    if(!this.valueExpression) {
+        return null;
+    }
+    else if (this.valueExpression.computed) {
+        var valueExpressionObj = new ValueExpression(this.valueExpression.property);
+
+        return this.valueExpression.object.name + '[' + valueExpressionObj.getValue() + ']';
+    } else if (this.valueExpression.operator) {
+        var valueExpressionObj = new ValueExpression(this.valueExpression.left);
+
+        return valueExpressionObj.getValue() + this.valueExpression.operator + this.valueExpression.right.name;
+    } else {
+        return this.valueExpression.name;
+    }
+};
+
+export {ValueExpression};
