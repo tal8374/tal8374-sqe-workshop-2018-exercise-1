c2a37b45044ab77e42d7a726438e6fa09de52a41
tal8374 <talivanov@gmail.com>
2018-11-12 15:24:51 +0200

    Fixing assignment declaration. Can assign numbers

--- src/js/variable-declaration-handler.js
+++ src/js/variable-declaration-handler.js
@@ -1,4 +1,4 @@
-import {insertLineHandler} from './common'
+import {insertLineHandler} from './common';
 
 function variableDeclaration(body, wrapper, lineNumber) {
     this.wrapper = wrapper;
@@ -9,26 +9,27 @@ function variableDeclaration(body, wrapper, lineNumber) {
 variableDeclaration.prototype.init = function () {
     var declarations = this.body.declarations;
 
-    for(let i = 0; i < declarations.length; i++) {
-        this.variableDeclarationHandler(declarations[i], this.lineNumber - 1)
+    for (let i = 0; i < declarations.length; i++) {
+        this.variableDeclarationHandler(declarations[i]);
     }
 
     this.wrapper.increaseLineNumber(this.lineNumber + 1);
 };
 
-variableDeclaration.prototype.variableDeclarationHandler = function(declaration, lineNumber) {
-    let payload = this.parseVariable(declaration, lineNumber);
+variableDeclaration.prototype.variableDeclarationHandler = function (declaration) {
+    let payload = this.parseVariable(declaration);
+    console.log(payload);
 
-    insertLineHandler(payload)
+    insertLineHandler(payload);
 };
 
-variableDeclaration.prototype.parseVariable = function parseVariable(declaration, lineNumber) {
+variableDeclaration.prototype.parseVariable = function parseVariable(declaration,) {
     return {
-        type : declaration.type,
-        name : declaration.id.name,
-        value : declaration.init ? declaration.init.value : 0,
-        lineNumber : this.wrapper.getLineNumber(),
-    }
+        type: declaration.type,
+        name: declaration.id.name,
+        value: declaration.init ? declaration.init.value : 0,
+        lineNumber: this.wrapper.getLineNumber(),
+    };
 };
 
 export {variableDeclaration};
