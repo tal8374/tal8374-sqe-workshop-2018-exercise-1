bcd08b3e13cad5cc86c08b9d53d5625253ae1f7f
tal8374 <talivanov@gmail.com>
2018-11-15 15:10:27 +0200

    Creating tests for function declaration.

---
+++ test/function-declaration-handler.test.js
@@ -0,0 +1,61 @@
+import assert from 'assert';
+import {FunctionDeclaration} from '../src/js/function-declaration-handler';
+import {parseCode} from '../src/js/code-analyzer';
+
+describe('While declaration handler', () => {
+    it('should check function statement with empty body', () => {
+        var parsedCode = parseCode('function functionName(){}');
+        var functionDeclaration = new FunctionDeclaration(parsedCode.body[0], null, 1);
+        var result = functionDeclaration.init();
+
+        assert.equal(
+            result,
+            'Success'
+        );
+    });
+
+    it('should check function statement with body', () => {
+        var parsedCode = parseCode('function functionName(){let a}');
+        var functionDeclaration = new FunctionDeclaration(parsedCode.body[0], null, 1);
+        var result = functionDeclaration.init();
+
+        assert.equal(
+            result,
+            'Success'
+        );
+    });
+
+    it('should check function statement with one argument', () => {
+        var parsedCode = parseCode('function functionName(arg1){}');
+        var functionDeclaration = new FunctionDeclaration(parsedCode.body[0], null, 1);
+        var result = functionDeclaration.init();
+
+        assert.equal(
+            result,
+            'Success'
+        );
+    });
+
+    it('should check function statement with two argument', () => {
+        var parsedCode = parseCode('function functionName(arg1, arg2){}');
+        var functionDeclaration = new FunctionDeclaration(parsedCode.body[0], null, 1);
+        var result = functionDeclaration.init();
+
+        assert.equal(
+            result,
+            'Success'
+        );
+    });
+
+    it('should get line number after increase', () => {
+        var parsedCode = parseCode('function functionName(){}');
+        var functionDeclaration = new FunctionDeclaration(parsedCode.body[0], null, 1);
+        functionDeclaration.increaseLineNumber();
+
+        assert.equal(
+            functionDeclaration.getLineNumber(),
+            2
+        );
+    });
+
+});
